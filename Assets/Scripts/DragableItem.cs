90c2a17871dd9a777e0dbcc16e9823c5d5247aa8
Roczil <conor1352@gmail.com>
2025-03-25 20:29:18 +0100

    Inventory Snap

--- Assets/Scripts/DragableItem.cs
+++ Assets/Scripts/DragableItem.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 public class DragableItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
+    public Image image;
     [HideInInspector] public Transform parentAfterDrag;
-    private Vector3 offset; // Store the initial offset
 
     public void OnBeginDrag(PointerEventData eventData)
     {
@@ -14,22 +15,18 @@ public class DragableItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEnd
         parentAfterDrag = transform.parent;
         transform.SetParent(transform.root);
         transform.SetAsLastSibling();
-
-        // Calculate the initial offset between the mouse and the object's center.
-        offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z - Camera.main.transform.position.z));
-
+        image.raycastTarget = false;
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        Debug.Log("Dragging");
-        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z - Camera.main.transform.position.z));
-        transform.position = mouseWorldPosition + offset;
+        transform.position = Input.mousePosition;
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
         Debug.Log("End Drag");
         transform.SetParent(parentAfterDrag);
+        image.raycastTarget = true;
     }
 }
--- Assets/Scripts/InventorySlot.cs
+++ Assets/Scripts/InventorySlot.cs
@@ -8,9 +8,11 @@ public class InventorySlot : MonoBehaviour, IDropHandler
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void OnDrop(PointerEventData eventData)
     {
+        if (transform.childCount == 0){
         GameObject dropped = eventData.pointerDrag;
         DragableItem dragableItem = dropped.GetComponent<DragableItem>();
         dragableItem.parentAfterDrag = transform;
+        }
 
     }
 }
